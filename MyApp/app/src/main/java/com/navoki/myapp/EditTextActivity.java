7c9de7eba546b5928b014256a4c10fa7a2d9a9b7
Shivam <shivamsrivastava.9@gmail.com>
2018-07-10 11:02:59 +0530

    imageview

--- MyApp/app/src/main/java/com/navoki/myapp/EditTextActivity.java
+++ MyApp/app/src/main/java/com/navoki/myapp/EditTextActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class EditTextActivity extends AppCompatActivity {
@@ -17,11 +18,13 @@ public class EditTextActivity extends AppCompatActivity {
     private EditText edtPass;
     private Button submit;
     private Button submit2;
+    private ImageView image;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_edittext);
 
         edtName = findViewById(R.id.edtName);
@@ -29,10 +32,19 @@ public class EditTextActivity extends AppCompatActivity {
         edtPass = findViewById(R.id.edtPass);
         submit = findViewById(R.id.submit);
         submit2 = findViewById(R.id.submit2);
-
+        image =  findViewById(R.id.image);
 
         submit.setOnClickListener(onClickListener);
-        //  submit2.setOnClickListener(onClickListener);
+
+
+        submit2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(EditTextActivity.this, "CLick", Toast.LENGTH_SHORT).show();
+                image.setImageResource(R.drawable.logo);
+
+            }
+        });
 
     }
 
@@ -43,25 +55,22 @@ public class EditTextActivity extends AppCompatActivity {
             String mobile = edtMobile.getText().toString();
             String pass = edtPass.getText().toString();
 
+            Log.e("MSG", name);
+
             if (!TextUtils.isEmpty(name)) // check if null or   ""
             {
                 if (!TextUtils.isEmpty(mobile) && mobile.length() == 10) {
 
                     if (!TextUtils.isEmpty(pass) && pass.length() >= 8) {
+                        // save in server
                         Toast.makeText(EditTextActivity.this, "User Registered", Toast.LENGTH_SHORT).show();
                     } else
                         Toast.makeText(EditTextActivity.this, "Enter pass greater than 8", Toast.LENGTH_SHORT).show();
-
                 } else
                     Toast.makeText(EditTextActivity.this, "Enter valid mobile", Toast.LENGTH_SHORT).show();
 
             } else
                 Toast.makeText(EditTextActivity.this, "Enter valid name", Toast.LENGTH_SHORT).show();
-
-
-            Log.e("FIELDS", name);
-            Log.e("FIELDS", mobile);
-            Log.e("FIELDS", pass);
 
         }
     };
